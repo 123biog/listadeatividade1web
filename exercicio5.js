607f8b9e41ce444dac6b4a980b9c42a5d57a4789
123biog <145069028+123biog@users.noreply.github.com>
2023-10-20 21:51:49 -0300

    lista 1

---
+++ exercicio5.js
@@ -0,0 +1,4 @@
+// exercicio 5
+const frase = "A beleza da natureza revela-se nos intricados detalhes e nas maravilhas ocultas de cada criatura, e é nessa descoberta incessante que encontro meu amor pela zoologia."
+console.log(frase)
+
---
+++ exercicio8.js
@@ -0,0 +1,4 @@
+// exercicio 8
+const listaCompras = ["suco", "uva", "arroz", "oleo"]
+console.log(listaCompras)
+
